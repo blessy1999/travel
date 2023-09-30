dcd2cc27b38b841ae37cf3f66c032a2060c98ef5
blessy1999 <74538221+blessy1999@users.noreply.github.com>
2023-09-30 23:07:24 +0530

    Update urls.py

--- newproject/newproject/newapp/urls.py
+++ newproject/newproject/newapp/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 urlpatterns = [
 
     path('',views.demo,name="demo"),
-    # path('add/',views.addition,name="addition"),
-    # path('contact/',views.contact,name="contact")
+    
 ]
