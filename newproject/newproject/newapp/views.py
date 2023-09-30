3c7bf1471c967b60cc8af35954b2e80fa4e8a208
blessy1999 <74538221+blessy1999@users.noreply.github.com>
2023-09-30 23:06:41 +0530

    Update views.py

--- newproject/newproject/newapp/views.py
+++ newproject/newproject/newapp/views.py
@@ -8,14 +8,3 @@ def demo(request):
     obj1=people.objects.all()
     return render(request,'index.html',{'result':obj,'res':obj1})
 
-# def addition(request):
-#     x=int(request.GET['num1'])
-#     y=int(request.GET['num2'])
-#     res=x+y
-#     sub=x-y
-#     mul=x*y
-#     div=x/y
-#     return render(request,'about.html',{'result':res,'subtraction':sub,'product':mul,'division':div})
-#
-# def contact(request):
-#     return HttpResponse("contact details")
